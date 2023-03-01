714014daa24c572432ebd32861b0bdb1fbdfd7fd
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-03-01 00:23:43 -0300

    adicionado teste rspec

--- lib/exercicio_1.rb
+++ lib/exercicio_1.rb
@@ -11,4 +11,43 @@ pode ser previamente definido no código;
 
 =end
 
+class Fibonacci
+    def fibonacci(n)
+        # Inicializei com os primeiros dois números da sequência
+        a, b = 0, 1
 
+        # Agora irei executar a sequencia de fibonacci até o n-ésimo número
+        n.times do
+            a, b = b, a + b
+        end
+
+        # Agora ele me retorna o n-ésimo numero da sequência de fibonacci
+        a
+
+    end
+
+    # solicitando a entrada do usuario
+
+    puts "Digite um número inteiro: "
+    num = gets.chomp.to_i
+
+    # Agora vou verificar se o número faz parte ou não da sequẽncia
+
+    pertence = false
+    i = 0
+    while fibonacci(i) <= num
+        if fibonacci(i) == num
+            pertence = true
+            break
+        end
+        i += 1
+    end
+
+    # Agora irei imprimir a mensagem informar se pertence ou não pertence
+
+    if pertence
+        puts "#{num} pertence a sequencia de fibonacci"
+    else
+        puts "#{num} não pertence a sequencia de fibonacci"
+    end
+end
